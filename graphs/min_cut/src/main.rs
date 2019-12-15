1892a8556295186f6f8e9594869dc028b204a663
rob-crane <28930454+Rob-Crane@users.noreply.github.com>
2019-12-14 21:47:22 -0800

    Add basic unit tests and debug.

--- graphs/min_cut/src/main.rs
+++ graphs/min_cut/src/main.rs
@@ -1,58 +1,106 @@
 mod min_cut {
-    use std::collections::HashMap;
-    struct Edge {
+    use std::collections::{HashMap, HashSet};
+    #[derive(Debug)]
+    pub struct Edge {
         nid1: u64,
         nid2: u64,
     }
-    struct Node {
-        eids: Vec<u64>,
+    #[derive(Debug)]
+    pub struct Node {
+        eids: HashSet<u64>,
     }
 
-    struct Graph {
-        nodes: HashMap<u64, Node>,
-        edges: HashMap<u64, Edge>,
+    impl Node {
+        pub fn new() -> Node {
+            return Node {eids : HashSet::new()};
+        }
+        pub fn add(&mut self, eid: u64) {
+          assert!(self.eids.insert(eid));
+        }
+        pub fn remove(&mut self, eid: u64) {
+          assert!(self.eids.remove(&eid));
+        }
+    }
+
+    #[derive(Debug)]
+    pub struct Graph {
+        pub nodes: HashMap<u64, Node>,
+        pub edges: HashMap<u64, Edge>,
         next_eid: u64,
     }
     impl Graph {
+        pub fn new() -> Self {
+            return Graph {
+                nodes: HashMap::new(),
+                edges: HashMap::new(),
+                next_eid: 0,
+            };
+        }
         pub fn add(&mut self, nid1: u64, nid2: u64) {
+            assert_ne!(nid1, nid2);
             self.add_edge(nid1, nid2);
-            self.update_node(nid1);
-            self.update_node(nid2);
+            self.maybe_add_node(nid1);
+            self.maybe_add_node(nid2);
             self.next_eid += 1;
         }
 
         fn add_edge(&mut self, nid1: u64, nid2: u64) {
             // Add Edge.
             let edge = Edge { nid1, nid2 };
-            if let Some(e) = self.edges.insert(self.next_eid, edge) {
+            if self.edges.insert(self.next_eid, edge).is_some() {
                 panic!("Already contains edge with ID {}", self.next_eid);
             }
         }
 
-        fn update_node(&mut self, nid: u64) {
-            let new_node = Node { eids: vec![self.next_eid] };
-            if let Some(n) = self.nodes.insert(nid, new_node) {
-                n.eids.push(self.next_eid);
+        fn maybe_add_node(&mut self, nid: u64) {
+            let opt_n = self.nodes.get_mut(&nid);
+            if let Some(n) = opt_n {
+                n.add(self.next_eid);
+            } else {
+                let mut n = Node::new();
+                n.add(self.next_eid);
+                self.nodes.insert(nid, n);
             }
         }
 
-        // Draw a random edge.
-        //fn random_eid(&self) -> u64 {
-        //}
-
-        // Merge the nodes of this edge.
-        fn merge_node(&mut self, eid: u64) {
-            let e = &self.edges.get(eid).unwrap();
+        // Merge node 2 of edge into node 1.
+        pub fn merge_edge(&mut self, eid: u64) {
+            let e = &self.edges.get(&eid).unwrap();
+            let nid1 = e.nid1;
+            let nid2 = e.nid2;
             // Redirect second node's edges to first.
-            let n2 = &mut self.nodes.get(e.nid2).unwrap();
-            for n2_eid in n2.eids {
-                self.redirect_edge_or_drop(n2_eid, e.nid2, e.nid1);
+            let n2 = &self.nodes.get(&nid2).unwrap();
+            let mut eids_to_add = Vec::<u64>::new();
+            let mut eids_to_del = Vec::<u64>::new();
+            for n2_eid in n2.eids.iter() {
+                if Graph::redirect_edge_or_drop(&mut self.edges, *n2_eid, nid2, nid1) {
+                    // Add edge ID to node.
+                    eids_to_add.push(*n2_eid);
+                } else {
+                    eids_to_del.push(*n2_eid);
+                }
+            }
+            let n = self.nodes
+                .get_mut(&nid1)
+                .unwrap();
+            for e in eids_to_add {
+                n.add(e);
+            }
+            for e in eids_to_del {
+                n.remove(e);
             }
         }
 
-        fn redirect_edge_or_drop(&mut self, eid: u64, nid_old: u64, nid_new: u64) {
-            let e = &mut self.edges.get(eid).unwrap();
-            let to_update, no_update;
+        // Redirects an edge's endpoints or drops it from edge map
+        // if redirection creates self-loop.
+        fn redirect_edge_or_drop(
+            edges: &mut HashMap<u64, Edge>,
+            eid: u64,
+            nid_old: u64,
+            nid_new: u64,
+        ) -> bool {
+            let e = edges.get_mut(&eid).unwrap();
+            let (to_update, no_update);
             if e.nid1 == nid_old {
                 to_update = &mut e.nid1;
                 no_update = &e.nid2;
@@ -60,14 +108,14 @@ mod min_cut {
                 to_update = &mut e.nid2;
                 no_update = &e.nid1;
             }
-            if no_update == nid_new {
+            if *no_update == nid_new {
                 // Drop Edge, merging would create self edge.
-                self.edges.drop(eid);
+                edges.remove(&eid);
+                return false;
             } else {
                 // Redirect Edge.
-                to_update = nid_new;
-                // Add eid to new node.
-                self.nodes.get(nid_new).unwrap().eids.push(eid);
+                *to_update = nid_new;
+                return true;
             }
         }
     }
@@ -79,4 +127,26 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_add() {
+        let mut g = min_cut::Graph::new();
+        g.add(1, 2);
+        g.add(1, 3);
+        g.add(2, 3);
+        assert_eq!(g.nodes.len(), 3);
+        assert_eq!(g.edges.len(), 3);
+    }
+
+    #[test]
+    fn test_merge() {
+        let mut g = min_cut::Graph::new();
+        g.add(1, 2);
+        g.add(2, 3);
+        g.add(1, 3);
+        println!("Edges: {:?}", g.edges);
+        g.merge_edge(2);
+        println!("Edges: {:?}", g.edges);
+    }
+
 }
