afe7c6eb6bb63f9b29a66b0b5a342d67d3b2ac60
rob-crane <28930454+Rob-Crane@users.noreply.github.com>
2019-12-24 22:47:32 -0800

    Complete recursive solution.

--- graphs/sccs/src/main.rs
+++ graphs/sccs/src/main.rs
@@ -11,7 +11,7 @@ fn get_stdin() -> io::Result<String> {
 
 fn main() {
     let input = get_stdin().unwrap();
-    let mut edge_map = scc::edge::EdgeMap::new();
+    let mut edge_map = scc::EdgeMap::new();
     for line in input.lines() {
         let edge_nids: Vec<u64> = line
             .split_whitespace()
@@ -20,7 +20,12 @@ fn main() {
         assert_eq!(edge_nids.len(), 2);
         edge_map.add(edge_nids[0], edge_nids[1]);
     }
-    println!("Done reading input.")
+    println!("Done reading input.");
+    let kfp = scc::KosarajuFirstPass::new(&edge_map);
+    println!("Completed first pass.");
+    let ksp = scc::KosarajuSecondPass::new(&edge_map, kfp.second_pass_ordering());
+    let rev_counts = ksp.reverse_sorted_counts();
+    println!("Top 5: {:?}", &rev_counts[..5]);
 }
 
 #[cfg(test)]
@@ -30,7 +35,7 @@ mod tests {
 
     #[test]
     fn test_add() {
-        let mut edge_map = scc::edge::EdgeMap::new();
+        let mut edge_map = scc::EdgeMap::new();
         edge_map.add(1, 2);
         edge_map.add(3, 2);
         edge_map.add(3, 4);
@@ -43,5 +48,40 @@ mod tests {
         let from_3: HashSet<&u64> = edge_map.nids_from(3).unwrap().collect();
         let from_3_exp = vec![&2, &4].into_iter().collect();
         assert_eq!(from_3, from_3_exp);
+    }
+
+    #[test]
+    fn test_first_pass() {
+        let mut edge_map = scc::EdgeMap::new();
+        edge_map.add(1, 2);
+        edge_map.add(2, 3);
+        let kfp = scc::KosarajuFirstPass::new(&edge_map);
+        let ordering : Vec<&u64> = kfp.second_pass_ordering().collect(); 
+        assert_eq!(ordering, vec![&3,&2,&1]);
+    }
+
+    #[test]
+    fn test_second_pass() {
+        let mut edge_map = scc::EdgeMap::new();
+        edge_map.add(1, 4);
+        edge_map.add(2, 8);
+        edge_map.add(3, 6);
+        edge_map.add(4, 7);
+        edge_map.add(5, 2);
+        edge_map.add(6, 9);
+        edge_map.add(7, 1);
+        edge_map.add(8, 5);
+        edge_map.add(8, 6);
+        edge_map.add(9, 7);
+        edge_map.add(9, 3);
+        let kfp = scc::KosarajuFirstPass::new(&edge_map);
+        let ksp = scc::KosarajuSecondPass::new(&edge_map, kfp.second_pass_ordering());
+        let rev_counts = ksp.reverse_sorted_counts();
+        assert_eq!(rev_counts[0], 3);
+        assert_eq!(rev_counts[1], 3);
+        assert_eq!(rev_counts[2], 3);
+        assert_eq!(rev_counts[3], 0);
+        assert_eq!(rev_counts[4], 0);
+        assert_eq!(rev_counts[5], 0);
     }
 }
--- graphs/sccs/src/scc.rs
+++ graphs/sccs/src/scc.rs
@@ -1,63 +1,185 @@
-pub mod edge {
-    use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet};
 
-    // Describe the connections to a single node.
-    // nids_to: Neighbors pointing to the node.
-    // nids_from: Neighbors the node points to.
-    struct EdgeList {
-        nids_to: HashSet<u64>,
-        nids_from: HashSet<u64>,
-    }
+// Describe the connections to a single node.
+// nids_to: Neighbors pointing to the node.
+// nids_from: Neighbors the node points to.
+struct EdgeList {
+    nids_to: HashSet<u64>,
+    nids_from: HashSet<u64>,
+}
 
-    impl EdgeList {
-        pub fn new() -> EdgeList {
-            EdgeList {
-                nids_to: HashSet::<u64>::new(),
-                nids_from: HashSet::<u64>::new(),
-            }
-        }
-        pub fn add_to(&mut self, nid_to: u64) {
-            assert!(self.nids_to.insert(nid_to));
-        }
-        pub fn add_from(&mut self, nid_from: u64) {
-            assert!(self.nids_from.insert(nid_from));
-        }
-        pub fn nids_to(&self) -> impl Iterator<Item = &u64> {
-            self.nids_to.iter()
-        }
-        pub fn nids_from(&self) -> impl Iterator<Item = &u64> {
-            self.nids_from.iter()
+impl EdgeList {
+    pub fn new() -> EdgeList {
+        EdgeList {
+            nids_to: HashSet::<u64>::new(),
+            nids_from: HashSet::<u64>::new(),
         }
     }
-
-    pub struct EdgeMap {
-        edges: HashMap<u64, EdgeList>,
+    pub fn add_to(&mut self, nid_to: u64) {
+        assert!(self.nids_to.insert(nid_to));
     }
+    pub fn add_from(&mut self, nid_from: u64) {
+        assert!(self.nids_from.insert(nid_from));
+    }
+    pub fn nids_to(&self) -> impl Iterator<Item = &u64> {
+        self.nids_to.iter()
+    }
+    pub fn nids_from(&self) -> impl Iterator<Item = &u64> {
+        self.nids_from.iter()
+    }
+}
 
-    impl EdgeMap {
-        pub fn new() -> EdgeMap {
-            EdgeMap {
-                edges: HashMap::<u64, EdgeList>::new(),
-            }
-        }
-        pub fn add(&mut self, nid_from: u64, nid_to: u64) {
-            (*self.edges.entry(nid_from).or_insert(EdgeList::new())).add_from(nid_to);
-            (*self.edges.entry(nid_to).or_insert(EdgeList::new())).add_to(nid_from);
-        }
-        // For a node, get all the nodes it points directly to.
-        pub fn nids_to(&self, nid: u64) -> Option<impl Iterator<Item = &u64>> {
-            if let Some(edge_list) = self.edges.get(&nid) {
-                return Some(edge_list.nids_to());
-            }
-            None
-        }
+pub struct EdgeMap {
+    edges: HashMap<u64, EdgeList>,
+}
 
-        // For a node, get all the nodes which point directly to it.
-        pub fn nids_from(&self, nid: u64) -> Option<impl Iterator<Item = &u64>> {
-            if let Some(edge_list) = self.edges.get(&nid) {
-                return Some(edge_list.nids_from());
-            }
-            None
+impl EdgeMap {
+    pub fn new() -> EdgeMap {
+        EdgeMap {
+            edges: HashMap::<u64, EdgeList>::new(),
         }
     }
-} // edge
+    pub fn add(&mut self, nid_from: u64, nid_to: u64) {
+        (*self.edges.entry(nid_from).or_insert(EdgeList::new())).add_from(nid_to);
+        (*self.edges.entry(nid_to).or_insert(EdgeList::new())).add_to(nid_from);
+    }
+    // For a node, get all the nodes it points directly to.
+    pub fn nids_to(&self, nid: u64) -> Option<impl Iterator<Item = &u64>> {
+        if let Some(edge_list) = self.edges.get(&nid) {
+            return Some(edge_list.nids_to());
+        }
+        None
+    }
+
+    // For a node, get all the nodes which point directly to it.
+    pub fn nids_from(&self, nid: u64) -> Option<impl Iterator<Item = &u64>> {
+        if let Some(edge_list) = self.edges.get(&nid) {
+            return Some(edge_list.nids_from());
+        }
+        None
+    }
+
+    pub fn nids(&self) -> impl Iterator<Item = &u64> {
+        self.edges.keys()
+    }
+
+    pub fn len(&self) -> usize {
+        self.edges.len()
+    }
+}
+
+// Track the nodes of graph visisted during DFS.
+struct VisitedMap {
+    visited: HashMap<u64, bool>,
+}
+
+impl VisitedMap {
+    pub fn new(edge_map: &EdgeMap) -> VisitedMap {
+        VisitedMap {
+            visited: edge_map.nids().map(|n| (*n, false)).collect(),
+        }
+    }
+
+    pub fn mark_visited(&mut self, nid: u64) {
+        let old = self.visited.insert(nid, true).unwrap();
+        assert!(!old); // Assert node wasn't previously visited.
+    }
+
+    pub fn is_visited(&self, nid: u64) -> bool {
+        *self.visited.get(&nid).unwrap()
+    }
+}
+
+pub struct KosarajuFirstPass<'a> {
+    edge_map: &'a EdgeMap,
+    visited: VisitedMap,
+    ordering: Vec<u64>,
+}
+
+impl<'a> KosarajuFirstPass<'a> {
+    pub fn new(edge_map: &'a EdgeMap) -> KosarajuFirstPass {
+        let mut kfp = KosarajuFirstPass {
+            edge_map,
+            visited: VisitedMap::new(edge_map),
+            ordering: Vec::<u64>::with_capacity(edge_map.len()),
+        };
+        for &nid in kfp.edge_map.nids() {
+            if !kfp.visited.is_visited(nid) {
+                kfp.dfs(nid);
+            }
+        }
+        kfp
+    }
+    pub fn second_pass_ordering(&self) -> impl Iterator<Item = &u64> {
+        self.ordering.iter().rev()
+    }
+    fn dfs(&mut self, nid: u64) {
+        self.visited.mark_visited(nid);
+        for &n in self.edge_map.nids_to(nid).unwrap() {
+            if !self.visited.is_visited(n) {
+                self.dfs(n);
+            }
+        }
+        self.ordering.push(nid);
+    }
+}
+
+struct ConnectedComponentCounts {
+    counts: HashMap<u64, usize>,
+}
+
+impl ConnectedComponentCounts {
+    pub fn new(edge_map: &EdgeMap) -> ConnectedComponentCounts {
+        ConnectedComponentCounts {
+            counts: edge_map.nids().map(|n| (*n, 0)).collect(),
+        }
+    }
+
+    pub fn increment(&mut self, nid: u64) {
+        let cnt = self.counts.get_mut(&nid).unwrap();
+        *cnt += 1;
+    }
+
+    pub fn counts(&self) -> impl Iterator<Item = &usize> {
+        self.counts.values()
+    }
+}
+
+pub struct KosarajuSecondPass<'a> {
+    edge_map: &'a EdgeMap,
+    visited: VisitedMap,
+    scc_counts: ConnectedComponentCounts
+}
+
+impl<'a, 'b> KosarajuSecondPass<'a>  {
+
+    pub fn new<T: Iterator<Item = &'b u64>> (edge_map: &'a EdgeMap, ordering: T) -> KosarajuSecondPass {
+        let mut ksp = KosarajuSecondPass{
+            edge_map,
+            visited: VisitedMap::new(edge_map),
+            scc_counts: ConnectedComponentCounts::new(edge_map)
+        };
+        for &nid_source in ordering {
+            if !ksp.visited.is_visited(nid_source) {
+                ksp.dfs(nid_source, nid_source);
+            }
+        }
+        ksp
+    }
+
+    pub fn reverse_sorted_counts(&self) -> Vec<usize> {
+        let mut ret : Vec<usize> = self.scc_counts.counts().map(|x| *x).collect();
+        ret.sort_unstable_by(|a, b| b.cmp(a));
+        ret
+    }
+
+    fn dfs(&mut self, nid: u64, nid_source: u64) {
+        self.visited.mark_visited(nid);
+        self.scc_counts.increment(nid_source);
+        for &n in self.edge_map.nids_from(nid).unwrap() {
+            if !self.visited.is_visited(n) {
+                self.dfs(n, nid_source);
+            }
+        }
+    }
+}
