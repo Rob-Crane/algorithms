bb00ccb39ca7e1e5830f4340ebb775ca3444630d
rob-crane <28930454+Rob-Crane@users.noreply.github.com>
2021-07-05 12:40:14 -0700

    Finish median_of_two.

--- median_of_two/README.md
+++ median_of_two/README.md
@@ -6,29 +6,51 @@ Given two sorted arrays, efficiently locate the median of the combined set.
 ## Approach
 
 ### Observations
-* If `N` (size of combined set) is odd, task is to locate element `N/2`. If size of combined set is even, task is to locate `N/2-1` and `N/2` so will generalize by focusing on finding the latter.
-* Let `ind(x)` be the index of a number `x` from its intput list and `rind(x)` be its "reverse index" (distance from last element). Let `IND(x)` and `RIND(x)` be the index in a (theoretical) combined sorted list.
+* If `N` (size of combined set) is odd, task is to locate element `N/2`. If size of combined set is even, task is to locate `N/2-1` and `N/2`.
+* Let `ind(x)` be the index of a number `x` from its input list and `rind(x)` be its "reverse index" (distance from last element). Let `IND(x)` and `RIND(x)` be the index in a (theoretical) combined sorted list.
   * *Note*: For a list of length `l` with element `x`, `rind(x) = l - 1 - ind(x)`.
-* Given `a` and `b`, drawn from separate lists, s.t. `a>b`, then `IND(a) >= ind(a) + ind(b) + 1` (`+1` because `b` must also come before `a`) and similarly, `RIND(b) >= rind(a) + rind(b) + 1`. So, let `MIND(a) = ind(a) + ind(b) + 1` and `MRIND(b) = rind(a) + rind(b) + 1`.
+* Suppose one element drawn from each list, let those be `a` and `b`  s.t. `a>=b`. Then `IND(a) >= ind(a) + ind(b) + 1` (`+1` because `b` must also come before `a`) and similarly, `RIND(b) >= rind(a) + rind(b) + 1`. So, let `MIND(a) = ind(a) + ind(b) + 1` and `MRIND(b) = rind(a) + rind(b) + 1`.
 * Elimination Rule: 
-  * If `MIND(a) >= N/2`, then can eliminate everything `>ind(a)` in `a`'s list. Equivalently, if `MRIND(b) >= RIND(N/2)`, can eliminate everything `<ind(b)` in its list.
-  * If `ind(a)`, `ind(b)` chosen so `ind(a) + ind(b) == N/2` then `MIND(a) == N/2 + 1` so elimination rule applies.
-  * With this choice of indicies, what can be said about reverse index of `b`?
+  * If `MIND(a) >= N/2`, then can eliminate everything `>ind(a)` in `a`'s list. Equivalently, if `MRIND(b) >= N-1-N/2`, can eliminate everything `<ind(b)` in its list.
+  * If `ind(a)`, `ind(b)` chosen so `ind(a) + ind(b) == N/2 - 1` then `MIND(a) == N/2` so elimination rule applies for the list from which `a` was drawn.
+  * With this choice of indicies, what about list from which `b` drawn?
 ```
-ind(a)+ind(b)==N/2
-len(a's list)-1-ind(a) + len(b's list)-1-ind(b) == N-2-N/2
-rind(a) + rind(b) == N-2-N/2
-rind(a) + rind(b) + 1 == N-1-N/2
-MRIND(b) == RIND(N/2)
-# So elimination criterion applies.
+ind(a)+ind(b)==N/2-1
+len(a's list)-1-ind(a) + len(b's list)-1-ind(b) == N-1-N/2
+rind(a) + rind(b) == N-1-N/2
+rind(a) + rind(b) + 1 == N-N/2
+MRIND(b) == N-N/2
+# So elimination criterion applies (but in other direction, eliminating lower elements).
 ```
+* Approach is to repeatedly apply elimination rule by selecting elements from each list (so that `ind(a) + ind(b) == N/2-1`), comparing them, and eliminating the elements above or below the selected elements.
+* Because of the restriction that `ind(x) + ind(y) == N/2-1`, the choice of one element determines the choice of the other. Furthermore, must ensure that the determined choice falls within range. Suppose `y` index of an element chosen from one list by some scheme and `x` is chosen from other list according to `x=N/2-1-y`. What are the restrictions on choice of `y`?
+```
+Restriction 1:
+
+To ensure x>=0:
+y<=N/2-1
+
+To ensure x <= len(x's list)-1
+y>=N/2-len(x's list)
+```
+* For each list, can ignore any elements at `ind > N/2` since they must fall after `N/2` in sorted ordering. Similarly, can ignore any elements at `rind > N/2+1` since they will fall before `N/2-1`. Therefore, this too creates the restriction on choices for `y`:
+```
+Restriction 2:
+len(y's list)-N/2-2<=y<=N/2
+```
+* So `Restriction 1` is one element more restrictive on `y`'s upper limit. On its lower limit:
+```
+[N/2-len(x's list)] - [len(y's list)-N/2-2]
+2N/2 -len(x's list) - len(y's list) + 2
+N - N + 2
+```
+* So `Restriction 1` is at most two elements more restrictive on `y`'s lower bound. But as long as `y`'s selection scheme doesn't choose either the bottom two elements or top element, `Restriction 1` is sufficient. This is the case in the scheme described below.
 
 ### Half List Elimination
-* Want to pick elements `i`, `j` so that `i+j = N/2`.
-* Such pairs will exist for every element in the shorter list and all elements in the longer list s.t. `for element i of shorter list N/2-i <= [length of shorter list]`.
-* Strategy is to choose "pivots" from smaller list which are the middle element of remaining interval and choose complementary `N/2` pivot from longer list (which will divide it roughtly in half). This strategy will terminate when the smaller list has an interval of length `<=2`.
+* Want to pick elements `i`, `j` so that `i+j = N/2-1`.
+* Strategy is to choose "pivots" from one list which are the middle element of remaining interval and choose complementary `N/2-1` pivot from other list (which will divide it roughtly in half). This strategy will terminate when the smaller list has an interval of length `<=2`.
 
 ### Final Step:
 * Half-list elimination ends when smaller list interval is length `<=2`. Task is to find the `N/2` value from a length `<=2` list and another list of arbitrary size.
-* Want to find the median of the total set from the intervals of possible values. If `k` values have been eliminated from the lower ranges of the two input lists, then the task is to find the `N/2-k`th order statistic from the intervals.
-* This can be accomplished in constant time by starting with the `k`th value from the arbitrary size interval and then examining the one or two values in the other interval to see if they fall before or after the `k`th value of the longer list.
+* Want to find the median of the total set from the intervals of possible values. If `k` values have been eliminated from the lower ranges of the two input lists, then the task is to find the `N/2-k`th order statistic from the remaining values.
+* This can be accomplished in constant time by starting with the `k`th value from the arbitrary size interval and moving backwards up to two times "inserting" the remaining two elements from the eliminated list. This is is similar to the merge step of Merge Sort but moving backwards up to two times from the `kth` value to the preceding elements or to the elements from the smaller list.
--- median_of_two/median_of_two.py
+++ median_of_two/median_of_two.py
@@ -1,66 +1,154 @@
-# from collections import namedtuple
-
 class Interval:
-    def __init__(self, values):
-        self.values = values
-        self.start = 0
-        self.end = len(self.values)
-
-    def __getitem__(self, idx):
-        assert idx >= self.start and idx < self.end
-        return self.values[idx]
-
-    def __len__(self):
-        return self.end - self.start
-
-# """
-# Container for results of half-list elimination when algorithm terminated.
-# Contains an arbitrary length instance of Interval corresponding to eliminated
-# values from one input and the one (x) or two (x, y) values remaining from other
-# input.
-# """
-# ReducedIntervals = namedtuple('ReducedIntervals', 'interval x y'])
-
-def get_reduced_intervals(values_i,
-                          values_j):
-    """
-    Iteratively reduce median possibility intervals until of the intervals
-    is length <= 2
-    Returns:
-      interval_short, interval_long where len(interval_short)<=2
-    """
-    N_2 = (len(values_i) + len(values_j)) // 2
+    def __init__(self, nums):
+        self.nums = nums
+        self.begin_idx = 0
+        self.end_idx = len(self.nums)
     
-    if len(values_i) <= len(values_j):
-        pivot_interval = Interval(values_i)
-        npivot_interval = Interval(values_j)
+    def at(self, i):
+        assert i >= self. begin_idx and i < self.end_idx
+        return self.nums[i]
+    
+    def front(self):
+        assert self.begin_idx >= 0 and self.begin_idx < self.end_idx
+        return self.nums[self.begin_idx]
+    
+    def last(self):
+        assert self.begin_idx >= 0 and self.end_idx <= len(self.nums)
+        return self.nums[self.end_idx-1]
+    
+    def empty(self):
+        assert self.begin_idx <= self.end_idx
+        return self.begin_idx == self.end_idx
+    
+    def eliminate_below(self, i):
+        assert i >= self.begin_idx and i < self.end_idx
+        self.begin_idx = i
+        
+    def eliminate_above(self, i):
+        assert i >= self.begin_idx and i <= self.end_idx
+        self.end_idx = i + 1
+        
+    def __len__(self):
+        assert self.begin_idx <= self.end_idx
+        return self.end_idx - self.begin_idx
+    
+    def __str__(self):
+        return str(self.nums[self.begin_idx:self.end_idx])
+
+def increment_down(interval_a, interval_b):
+    if interval_a.empty() and interval_b.empty():
+        ret = None
     else:
-        pivot_interval = Interval(values_j)
-        npivot_interval = Interval(values_i)
-
-    while len(pivot_interval) >= 2:
-        pivot_idx = pivot_interval.start + len(pivot_interval) // 2
-        assert pivot_idx < pivot_interval.end
-        npivot_idx = N_2 - pivot_idx
-        assert npivot_idx < npivot_interval.end
-
-        if pivot_interval[pivot_idx] < npivot_interval[npivot_idx]:
-            pass
+        if interval_a.empty():
+            decrement_interval = interval_b
+        elif interval_b.empty():
+            decrement_interval = interval_a
+        elif interval_a.last() > interval_b.last():
+            decrement_interval = interval_a
         else:
-            pass
+            decrement_interval = interval_b
+        ret = decrement_interval.last()
+        decrement_interval.end_idx-=1
+    return ret
+    
 
-        if len(npivot_interval) < len(pivot_interval):
-            pivot_interval, npivot_interval = npivot_interval, pivot_interval
+# Find the N/2 and N/2-1 values from two intervals where one is arbitrary length
+# and the other is size <= 2. Returns (value at N/2-1, value at N/2).
+def find_medians_from_reduced(interval1, interval2, n_2):
+    
+    # Algorithm is linear in length of shorter interval. Let x be the shorter
+    # interval and y the longer.
+    if len(interval1) < len(interval2):
+        x = interval1
+        y = interval2
+    else:
+        x = interval2
+        y = interval1
+    
+    # t is the index of n/2 in y if no more elements from x preceded it.
+    
+    m1, m2 = None, None
+    
+    # From elimination, N/2 and N/2-1 positions must remain in intervals.
+    # Case 1 begins by assuming N/2 occurs in y, then "adds" non-eliminated
+    # elements from x and tracks the N/2th and N/2-1th positions.
+    
+    # Won't add eliminated elements from x so compute index of target N/2
+    # after adding non-eliminated elements.
+    t = n_2 - x.begin_idx
+    assert t >= y.begin_idx # Indicate problematic elimination.
+    # Case 1: 
+    if t < y.end_idx:
+        
+        # N/2 can't come before t so can eliminate everything above.
+        y.end_idx = t+1
+        
+        # Only "add" elements from x < y[t].
+        while not x.empty() and x.last() > y.at(t):
+            x.end_idx-=1
+        steps_back = len(x) + 2
+    # Case 2:
+    else:
+        steps_back = x.end_idx + y.end_idx - n_2 + 1
+        assert steps_back > 0
+        
+    for _ in range(steps_back):
+        m2 = m1
+        m1 = increment_down(x, y)
+    return m1, m2
+        
+class Solution:
+    
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        
+        
+        n_2 = (len(nums1) + len(nums2)) // 2
+        interval1 = Interval(nums1)
+        interval2 = Interval(nums2)
+        
+        # Works:
+        # interval1.begin_idx = max(0, len(nums1) - n_2 - 2)
+        # interval2.begin_idx = max(0, len(nums2) - n_2 - 2)
+        # interval1.end_idx = min(n_2+1, interval1.end_idx)
+        # interval2.end_idx = min(n_2+1, interval2.end_idx)
+        
+        interval1.begin_idx = max(0, n_2 - len(nums2)-1)
+        interval2.begin_idx = max(0, n_2 - len(nums1)-1)
+        interval1.end_idx = min(n_2+1, interval1.end_idx)
+        interval2.end_idx = min(n_2+1, interval2.end_idx)
 
-    return pivot_interval, npivot_interval
-
-
-def find_from_reduced(pivot_interval, npivot_interval):
-    """
-    Find the median from intervals where shorter is length <= 2.
-    """
-    pass
-
-
-def find_median(values_i, values_j):
-    return find_from_reduced(*get_reduced_intervals(values_i, values_j))
+        print('b1:',
+              interval1.begin_idx,
+              'e1:',
+              interval1.end_idx,
+              'b2:',
+              interval2.begin_idx,
+              'e2:',
+              interval2.end_idx,)
+        print(nums1, nums2)
+        
+        while len(interval1) > 2 and len(interval2) > 2:
+            # Save starting interval lengths.
+            len1 = len(interval1)
+            len2 = len(interval2)
+            
+            # Choose middle index of interval1.
+            idx1 = interval1.begin_idx + (interval1.end_idx - interval1.begin_idx) // 2
+            # Choose interval2 index so sum of intervals is N/2 - 1
+            idx2 = n_2 - 1 - idx1
+            if interval1.at(idx1) < interval2.at(idx2):
+                interval1.eliminate_below(idx1) # TODO: is this right?
+                interval2.eliminate_above(idx2)
+            else:
+                interval2.eliminate_below(idx2)
+                interval1.eliminate_above(idx1)
+                
+            # Infinite loop guard.
+            assert len1 != len(interval1) or len2 != len(interval2)
+            
+        m1, m2 = find_medians_from_reduced(interval1, interval2, n_2)
+        
+        if (len(nums1) + len(nums2)) % 2 == 0:
+            return (m1 + m2) / 2
+        else:
+            return m2
